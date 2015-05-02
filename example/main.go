d6886bfe6dbda1722c8e916bdcd71e7b348c66f0
peterliu <peterjliu@gmail.com>
2015-05-02 14:51:21 -0700

    added arduino code to example

--- example/main.go
+++ example/main.go
@@ -1,4 +1,7 @@
 // Program to read and log the Arduino serial output.
+// Tested with arduinocode/temp36.ino loaded onto a Uno.
+// This reports the temperature in celsius and voltage from a Temp36 sensor
+// in CSV format.
 package main
 
 import (
@@ -15,8 +18,9 @@ func check(err error) {
 
 func main() {
 	c, err := goserial.NewClient("/dev/cu.usbmodem1421", 9600)
+	defer c.Close()
 	check(err)
-	for {
+	for i := 0; i < 5; i++ {
 		line, err := c.ReadLine()
 		check(err)
 		log.Print(line)
